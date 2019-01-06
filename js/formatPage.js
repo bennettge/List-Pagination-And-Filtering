0460a7c2726368dc4c54d1e9210c58d85d9d83ce
George Austin Bennett <bennettge@thayer.org>
2019-01-06 01:08:22 -0500

    Updated Version (Refactored and includes exceeds)

--- js/formatPage.js
+++ js/formatPage.js
@@ -3,88 +3,12 @@ FILE DESCRIPTION: This file holds all information/formatted HTML to be accessed
 by the script.js file (which adds interactivity)
 */
 
-// Makes a student object based on the info in the li elements
-function makeStudent(currStudent) {
-
-  // Gets student information
-  const studentDetails = currStudent.firstElementChild.children;
-  const studentImage = studentDetails[0].getAttribute("src");
-  const studentName = studentDetails[1].textContent;
-  const studentEmail = studentDetails[2].textContent;
-  const studentDate = currStudent.lastElementChild.firstElementChild.textContent;
-
-  // Makes the student object
-  const student = {image: studentImage,
-                   name: studentName,
-                   email: studentEmail,
-                   joinDate: studentDate};
-
-  return student;
-
-};
-
-// Makes array of student objects
-function makeStudentArray() {
-
-  const studentData = [];
-
-  // Gets all students on the page
-  const listOfStudents = document.querySelector(".student-list").children;
-
-  // Formats students into objects and adds them to the studentData array
-  for (let index = 0; index < listOfStudents.length; index += 1) {
-    const currStudent = makeStudent(listOfStudents[index]);
-    studentData.push(currStudent);
-  }
-
-  return studentData;
-
-};
-
-// Makes an li element with given class name
-function makeLi (liClassName) {
-
-  const liElement = document.createElement("li");
-  liElement.className = liClassName;
-  return liElement;
-
-};
-
 // Makes a div element with given class name
 function makeDiv (divClassName) {
 
   const newDiv = document.createElement("div");
   newDiv.className = divClassName;
   return newDiv;
-
-};
-
-// Makes an img element with given class name and source
-function makeImg (imgClassName, imgSource) {
-
-  const newImg = document.createElement("img");
-  newImg.className = imgClassName;
-  newImg.src = imgSource;
-  return newImg;
-
-};
-
-// Makes a h3 element with given text
-function makeH3 (h3TextContent) {
-
-  const newH3 = document.createElement("h3");
-  newH3.textContent = h3TextContent;
-  return newH3;
-
-};
-
-// Makes a span element with given class name and text content
-function makeSpan (spanClassName, spanTextContent) {
-
-  const newSpan = document.createElement("span");
-  newSpan.className = spanClassName;
-  newSpan.textContent = spanTextContent;
-  return newSpan;
 
 };
 
@@ -97,60 +21,14 @@ function makeButton(buttonTextContent) {
 
 };
 
-// Makes a student-details div
-function makeStudentDetailsDiv(currStudent) {
+// Makes the search bar
+function makeSearchBar () {
 
-  const studentDetailsDiv = makeDiv("student-details");
+  const searchBar = document.createElement("form");
+  searchBar.appendChild(makeInput());
+  return searchBar;
 
-  // Makes children tags for the studentDetails div and appends them to the div
-  studentDetailsDiv.appendChild(makeImg("avatar", currStudent.image));
-  studentDetailsDiv.appendChild(makeH3(currStudent.name));
-  studentDetailsDiv.appendChild(makeSpan("email", currStudent.email));
-
-  return studentDetailsDiv;
-
-};
-
-// Makes a joined-details div
-function makeJoinedDetailsDiv(joinDate) {
-
-  const joinedDetailsDiv = makeDiv("joined-details");
-
-  // Makes a child tag for the joinedDetails div and appends them to the div
-  joinedDetailsDiv.append(makeSpan("date", joinDate));
-
-  return joinedDetailsDiv;
-
-};
-
-// Makes a student li element
-function formatStudent(currStudent) {
-
-  const liElement = makeLi("student-item cf");
-
-  // Appends student-details and joined-details div to the li element
-  liElement.appendChild(makeStudentDetailsDiv(currStudent));
-  liElement.appendChild(makeJoinedDetailsDiv(currStudent.joinDate));
-
-  return liElement;
-
-};
-
-// Makes a list of student li elements
-function makeListOfLis() {
-
-  const listOfLiElements = [];
-
-  // Loops through all student objects, converts them to student lis,
-  // And appends them to the listOfLiElements array
-  for (let index = 0; index < studentData.length; index += 1) {
-    const currLiElement = formatStudent(studentData[index]);
-    listOfLiElements.push(currLiElement);
-  }
-
-  return listOfLiElements;
-
-};
+}
 
 // Makes pagination buttons
 function makePaginationButtons(numStudents) {
@@ -183,11 +61,41 @@ function makeButtonsDiv () {
 
   return buttonsDiv;
 
+};
+
+// Adds an element with given
+function addAttribute(element, nameOfAttribute, attributeValue) {
+
+  // Creates an attribute and sets its value to the given value
+  const attribute = document.createAttribute(nameOfAttribute);
+  attribute.value = attributeValue;
+
+  // Adds the attricbute to the element
+  element.setAttributeNode(attribute);
+
+  return element;
+
+}
+
+function makeInput() {
+
+  const input = document.createElement("input");
+
+  // Adds attributes to the input element
+  addAttribute(input, "type", "text");
+  addAttribute(input, "name", "name");
+  addAttribute(input, "placeholder", "Search for someone...");
+
+  // This attribute calls the onKeyPressed function after a character is typed
+  addAttribute(input, "onkeyup", "onKeyPressed()");
+
+  return input;
+
 }
 
 // Initializes important data that is accessed by the script.js file
-const studentData = makeStudentArray();
-const formattedLiElements = makeListOfLis();
-const paginationButtons = makePaginationButtons(studentData.length);
-const buttonsDiv = makeButtonsDiv();
 const studentListUl = document.querySelector(".student-list");
+const formattedLiElements = document.querySelector(".student-list").children;
+const paginationButtons = makePaginationButtons(formattedLiElements.length);
+const buttonsDiv = makeButtonsDiv();
+const searchBar = makeSearchBar();
--- js/script.js
+++ js/script.js
@@ -19,8 +19,8 @@ function removeCurrentLinks() {
 
   // Loops through all elements in linksToRemove and removes the from the ul
   for (let index = 0; index < numChildrenToRemove; index += 1) {
-    const currStudent = linksToRemove[0];
-    studentListUl.removeChild(currStudent);
+    const currStudent = linksToRemove[index];
+    currStudent.style.display = "none";
   }
 
 };
@@ -29,20 +29,45 @@ function removeCurrentLinks() {
 function placePaginatedLinks(pageNumber) {
 
   // Determines the index to start from in the formattedLiElements array
-  const startIndex = (pageNumber-1)*10
+  const startIndex = (pageNumber-1)*10;
 
   // Determines the number of li elements based on page length
   let numButtons = 10;
-  if (startIndex+10 >= studentData.length) {
-    numButtons = studentData.length % 10;
+  if (startIndex+10 >= linksToDisplay.length) {
+    numButtons = linksToDisplay.length % 10;
   }
+
+  // Determines if there are no matching links
+  displayIfNoLinks();
 
   // Loops though a slice of formattedLiElements and adds them to studentListUl
   for (let index = startIndex; index < startIndex + numButtons; index += 1) {
-    studentListUl.appendChild(formattedLiElements[index]);
+    linksToDisplay[index].style.display = "";
   }
 
 };
+
+// Displays if there aren't any valid search results
+function displayIfNoLinks() {
+
+  // Used in showing if there are no results
+  const page = document.querySelector(".page");
+
+  // If there is already an h1 tag, it is removed
+  if (page.lastElementChild.tagName === "H1") {
+    page.removeChild(page.lastElementChild);
+  }
+
+  // If there are 0 links that match the search, then a message is outputted
+  if (linksToDisplay.length === 0) {
+    const noTextMessage = document.createElement("h1");
+    noTextMessage.textContent = "No Search Matches!";
+
+    // Makes h1 tag for output
+    page.appendChild(noTextMessage);
+  }
+}
+
 
 // Places button elements into the buttonsDiv div
 function placeButtons(paginationButtons) {
@@ -88,9 +113,43 @@ function replaceLinks (buttonNumber) {
   removeCurrentLinks();
 
   // Adds the desired li elements to the ul
-  placePaginatedLinks(buttonNumber);
+  placePaginatedLinks(buttonNumber, linksToDisplay);
 
 };
+
+// Puts a search bar in the page header div
+function displaySearchBar() {
+  const pageHeader = document.getElementsByClassName("page-header cf")[0];
+  pageHeader.appendChild(searchBar);
+};
+
+// Checks if a the current input in the form currently matches a student's name
+function soFarMatches(currentInput, studentName) {
+
+  // Sentinel value for making sure it matches
+  let soFarMatches = true;
+
+  // Changes both local copies of the strings to lower case for evaluation
+  currentInput = currentInput.toLowerCase();
+  studentName = studentName.toLowerCase();
+
+  // If the search is greater then the student's name it's always false
+  if (currentInput > studentName) {
+    soFarMatches = false;
+  }
+
+  // Checks each character and if they don't match then it sets the bool to false
+  else {
+    for (let index = 0; index < currentInput.length; index += 1) {
+      if (currentInput.charAt(index) != studentName.charAt(index)) {
+        soFarMatches = false;
+      }
+    }
+  }
+
+  return soFarMatches;
+
+}
 
 // Adds a button event listener to the parent of the buttons and changes
 // Based on the button clicked because of event bubbling
@@ -103,13 +162,71 @@ buttonsDiv.addEventListener("click", (event) => {
 
 });
 
+// Function for when a key is inputted into the search bar
+function onKeyPressed () {
+
+  // Gets the current text in the space bar
+  const currInput = searchBar.firstElementChild.value;
+
+  // Gets all matching li elements
+  const nameMatches = findNameMatches(currInput);
+
+  // Hides extra buttons
+  hideButtons(nameMatches.length);
+
+  // Sets the linksToDisplay to the matches for output
+  linksToDisplay = nameMatches;
+
+  // Shows first page of output
+  replaceLinks(1);
+}
+
+// Finds all links that have an h3 text that matches the text in the search bar
+function findNameMatches (currInput) {
+
+  const nameMatches = []
+
+  // Loops though formattedLiElements (All of the list elements)
+  for (let index = 0; index < formattedLiElements.length; index += 1) {
+
+    // Gets the text content of the list element to check
+    const nameToCheck = document.querySelectorAll("h3")[index].textContent;
+
+    // If it currently matches, it adds it to the nameMatches array and displays
+    if (soFarMatches(currInput, nameToCheck)) {
+      nameMatches.push(formattedLiElements[index]);
+      formattedLiElements[index].style.display = "";
+    }
+
+    // If it doesn't it hides the element
+    else {
+      formattedLiElements[index].style.display = "none";
+    }
+  }
+
+  return nameMatches;
+
+}
+
+// Hides search buttons that aren't needed
+function hideButtons (numCurrMatches) {
+
+  // Loops through all of the pagination buttons and hides them if not needed
+  for (let index = 0; index < paginationButtons.length; index += 1) {
+    if (index*10 < numCurrMatches) {
+      paginationButtons[index].style.display = "";
+    } else {
+      paginationButtons[index].style.display = "none";
+    }
+  }
+
+}
+
+// Used in determining whether to separate search results or not
+let linksToDisplay = formattedLiElements;
+
 // Calls functions that set up the first view of the page
 replaceLinks(1);
 resetButtonColors();
 placeButtons(paginationButtons);
-
-/*
-Search Bar Idea: In editing state, call a function that updates the pages/buttons
-
-Make function soFarMatches that compares a name to current string
-*/
+displaySearchBar();
